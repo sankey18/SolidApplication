2478a0d41b76c3b2a1f1253e72001953e9319232
nileshbharsawade24 <nileshbharsawade24@gmail.com>
2025-08-22 22:06:43 +0530

    Naming Changes

--- SolidApplication/src/main/java/com/example/solid/model/Employee.java
+++ SolidApplication/src/main/java/com/example/solid/model/Employee.java
@@ -3,7 +3,6 @@ package com.example.solid.model;
 import com.example.solid.enumUtils.Status;
 import lombok.*;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
